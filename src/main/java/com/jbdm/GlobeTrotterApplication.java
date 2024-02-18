9e9fa86cbbc972bbf2d5a3f077012abc38b24acb
Digvijay Jatkar <jatkardigvijay@gmail.com>
2024-02-18 01:37:34 +0000

    initial commit

---
+++ src/main/java/com/jbdm/GlobeTrotterApplication.java
@@ -0,0 +1,15 @@
+package com.jbdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobeTrotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobeTrotterApplication.class, args);
+		
+		System.out.println("Hello World");
+	}
+
+}
